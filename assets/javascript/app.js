0da86ab52d818dd25a0e68220883c0d9c527d7e4
Jimin Huh <jiminhuh95@gmail.com>
2019-02-05 20:37:28 -0600

    added firebase initialization

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCswD9r_NwpQdqKraJ6hIsgSsJ3V8YuFJo",
+    authDomain: "group5project1-540d2.firebaseapp.com",
+    databaseURL: "https://group5project1-540d2.firebaseio.com",
+    projectId: "group5project1-540d2",
+    storageBucket: "",
+    messagingSenderId: "261770648936"
+  };
+  firebase.initializeApp(config);
