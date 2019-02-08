d68410c9bc8f346179afb9366760951be202421b
randolphbailey <randolphbailey@gmail.com>
2019-02-07 18:49:33 -0600

    move javascript and CSS related to Google maps to javascript.js and style.css files; clean up HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,5 +6,30 @@ var config = {
     projectId: "group5project1-540d2",
     storageBucket: "",
     messagingSenderId: "261770648936"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
+
+/*
+GOOGLE MAPS CODE
+*/
+
+var map;
+var service;
+
+function initMap() {
+//Set Austin latitude and longitude
+var austin = new google.maps.LatLng(30.27,-97.74);
+
+//Initialize map
+map = new google.maps.Map(document.getElementById('map'), {
+center: austin,
+zoom: 10
+});
+
+//Initialize Places functionality
+service = new google.maps.places.PlacesService(map);
+}
+
+/*
+END GOOGLE MAPS CODE
+*/
