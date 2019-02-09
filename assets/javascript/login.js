431d305c822c98625ba4ddd7d5f11bfc0e77fa89
Jimin Huh <jiminhuh95@gmail.com>
2019-02-09 12:39:07 -0600

    google sign in

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -20,7 +20,7 @@ var config = {
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
-        console.log(user);
+        $("#google").text("Signed in!"); 
       }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
@@ -37,25 +37,6 @@ var config = {
 
 
 
-  firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // ...
-    }
-    // The signed-in user info.
-    var user = result.user;
-    console.log(user);
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
 
 
 
