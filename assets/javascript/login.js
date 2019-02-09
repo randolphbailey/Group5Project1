83c4f15fe85fe92b43811f03bebe907376c39ef3
Jimin Huh <jiminhuh95@gmail.com>
2019-02-09 11:36:46 -0600

    testing6

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -24,17 +24,22 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
   });
 
   var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        window.location.href = "https://jiminhuh.github.io/Tweetmaptestrepo/main"
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return false;
-      }
-    },
+    // callbacks: {
+    //   signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+
+    //     console.log("WHEN AM I HAPPENING")
+    //       console.log(authResult);
+
+    //       console.log(redirectUrl);
+    //     // User successfully signed in.
+    //     // Return type determines whether we continue the redirect automatically
+    //     // or whether we leave that to developer to handle.
+    //     return true;
+    //   }
+    // },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'redirect',
+    signInSuccessUrl: "https://jiminhuh.github.io/Tweetmaptestrepo/main",
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
