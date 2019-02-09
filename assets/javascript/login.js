389cd56911baadf96c7ae3459d4c1c8074377258
Jimin Huh <jiminhuh95@gmail.com>
2019-02-09 12:58:53 -0600

    button populated with sign in popups

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -11,10 +11,14 @@ var config = {
   firebase.initializeApp(config);
 
 
-  var provider = new firebase.auth.GoogleAuthProvider();
+  var google = new firebase.auth.GoogleAuthProvider();
+  var twitter = new firebase.auth.TwitterAuthProvider();
+  var facebook = new firebase.auth.FacebookAuthProvider();
+  var github = new firebase.auth.GithubAuthProvider();
 
-  $("#google-button").on("click", function () {
-    firebase.auth().signInWithPopup(provider).then(function(result) {
+
+  $("#google-button").on("click",() => {
+    firebase.auth().signInWithPopup(google).then(function(result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         // The signed-in user info.
@@ -33,8 +37,65 @@ var config = {
 
   })
 
+  $("#facebook-button").on("click", () => {
+    firebase.auth().signInWithPopup(facebook).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  })
 
+  $("#twitter-button").on("click", () => {
+    firebase.auth().signInWithPopup(twitter).then(function(result) {
+        // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+        // You can use these server side with your app's credentials to access the Twitter API.
+        var token = result.credential.accessToken;
+        var secret = result.credential.secret;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+  })
 
+  $("#github-button").on("click", () => {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  })
+  
 
 
 
