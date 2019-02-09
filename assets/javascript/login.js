feebd6b9dd2d4c7869890429c728752fe9ce27f3
Jimin Huh <jiminhuh95@gmail.com>
2019-02-09 13:13:28 -0600

    deleted twitter and facebook login

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -11,9 +11,8 @@ var config = {
   firebase.initializeApp(config);
 
 
+  // Initialize Providers
   var google = new firebase.auth.GoogleAuthProvider();
-  var twitter = new firebase.auth.TwitterAuthProvider();
-  var facebook = new firebase.auth.FacebookAuthProvider();
   var github = new firebase.auth.GithubAuthProvider();
 
 
@@ -35,46 +34,6 @@ var config = {
         // ...
       });
 
-  })
-
-  $("#facebook-button").on("click", () => {
-    firebase.auth().signInWithPopup(facebook).then(function(result) {
-        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
-  })
-
-  $("#twitter-button").on("click", () => {
-    firebase.auth().signInWithPopup(twitter).then(function(result) {
-        // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
-        // You can use these server side with your app's credentials to access the Twitter API.
-        var token = result.credential.accessToken;
-        var secret = result.credential.secret;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-    }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-    });
   })
 
   $("#github-button").on("click", () => {
