650ea1fb72f24b613c62aa50e216704a54a96784
Jimin Huh <jiminhuh95@gmail.com>
2019-02-09 10:18:18 -0600

    seperated javascript files, new login.js file.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,57 +17,6 @@ console.log($('#container2').tweetie({
 //   end twitterr code
 
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCswD9r_NwpQdqKraJ6hIsgSsJ3V8YuFJo",
-    authDomain: "group5project1-540d2.firebaseapp.com",
-    databaseURL: "https://group5project1-540d2.firebaseio.com",
-    projectId: "group5project1-540d2",
-    storageBucket: "",
-    messagingSenderId: "261770648936"
-  };
-
-firebase.initializeApp(config);
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-  // Firebase UI setup
-  ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID
-    ],
-    // Other config options...
-  });
-
-  var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return false;
-      }
-    },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'redirect',
-    signInSuccessUrl: "https://www.google.com",
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    ]
-  };
-  
-
-firebase.initializeApp(config);
-
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
-
 /*
 GOOGLE MAPS CODE
 */
