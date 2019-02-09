c603e6922996cebdcccef91ff271aa8c074280a0
Jimin Huh <jiminhuh95@gmail.com>
2019-02-09 12:29:36 -0600

    testing16

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -14,24 +14,26 @@ var config = {
   var provider = new firebase.auth.GoogleAuthProvider();
   console.log(firebase.auth().currentUser);
 
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+  $("#google").on("click", function () {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  })
 
-  //firebase.auth().signInWithRedirect(provider);
+
 
 
   firebase.auth().getRedirectResult().then(function(result) {
