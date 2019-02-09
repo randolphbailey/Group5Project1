7896e6fb369ad7d68ad6e32f0a7bbf751e7e8e37
Jimin Huh <jiminhuh95@gmail.com>
2019-02-09 12:49:06 -0600

    added google sign in pop up

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -19,7 +19,7 @@ var config = {
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
-        $("#google").val("Signed in!"); 
+        console.log("Sign in successful");
       }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
