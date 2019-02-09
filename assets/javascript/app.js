61372cee5e2c8ce58d1a39203157d2a97b989d35
randolphbailey <randolphbailey@gmail.com>
2019-02-09 13:03:18 -0600

    cleanup javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,31 +18,16 @@ console.log($('#container2').tweetie({
 
 
 /*
-GOOGLE MAPS CODE
+START GOOGLE MAPS CODE
 */
-var searchBox, query;
-$("#submit").on("click", function() {
-  searchBox = $("#fd").val();
-  $("#fd").val("");
-});
 
-
+//define some global variables requried by Google API
 var map;
 var service;
 var infowindow;
 
-function createMarker(place) {
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-
+//Main function that Google API will look for and run on page load
+//Most code related to the map should go inside this function
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 30.266350, lng: -97.744267},
@@ -53,7 +38,6 @@ function initMap() {
   // Create the search box and link it to the UI element.
   var input = document.getElementById('fd');
   var searchBox = new google.maps.places.SearchBox(input);
-  //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
